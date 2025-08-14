1750a58b5427dfd870e4a3be699bcde1dc48b1d6
Rahma-Atef527 <roroaatef527@gmail.com>
2025-08-14 23:35:39 +0300

    Update solution.sh

--- Task-3/solution.sh
+++ Task-3/solution.sh
@@ -1,1 +1,18 @@
+sudo useradd hero
+sudo useradd intruder
+sudo userdel intruder
+sudo groupadd warriors
+sudo groupadd healers
+sudo usermod -aG warriors hero
+sudo useradd sage
+sudo usermod -aG healers sage
+sudo usermod -aG healers hero
+touch treasure.txt
+nano treasure.txt
+chmod 000 treasure.txt
+sudo chown hero treasure.txt
+sudo chmod 600 treasure.txt
+sleep 100 &
+ps
+kill -19 15306
 
